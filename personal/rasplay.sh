6709051a5f7a03c9b36dd876a9e14eeff737607a
tabascoes <87574268+tabascoes@users.noreply.github.com>
2021-09-27 14:17:32 +0900

    Update rasplay.sh

--- personal/rasplay.sh
+++ personal/rasplay.sh
@@ -4,14 +4,15 @@ cd
 
 sudo apt update
 sudo apt full-upgrade -y
+sudo apt install xscreensaver -y
 
 git clone https://github.com/FD-/RPiPlay.git
 cd RPiPlay
 
-sudo apt-get install cmake
-sudo apt-get install libavahi-compat-libdnssd-dev
-sudo apt-get install libplist-dev
-sudo apt-get install libssl-dev
+sudo apt install -y cmake
+sudo apt install -y libavahi-compat-libdnssd-dev
+sudo apt install -y libplist-dev
+sudo apt install -y libssl-dev
 mkdir build
 cd build
 cmake ..
@@ -42,9 +43,8 @@ EOF
 
 sudo systemctl daemon-reload
 sudo systemctl enable rpiplay
-sudo systemctl start rpiplay
 
-cat <<EOF | sudo tee $HOME/wallpaper.sh
+cat <<EOF | tee $HOME/wallpaper.sh
 #!/bin/bash
 pcmanfm --wallpaper-mode=fit
 x=1
@@ -53,8 +53,34 @@ do
   pcmanfm -w $HOME/wallpaper/$x.jpg
   if [ x = $(ls /home/pi/wallpaper/ -U1 | wc -l) ]; then
     x=1
+  else
+    x=$((x+1))
   fi
+  sleep $1
 done
 EOF
 
 sudo chmod 777 $HOME/wallpaper.sh
+echo 1 > $HOME/wallpaper-option.txt
+
+cat <<EOF | sudo tee /etc/systemd/system/wallpaper.service
+[Unit]
+Description=Wallpaper
+After=network.target
+StartLimitIntervalSec=30
+
+[Service]
+Type=simple
+Restart=always
+RestartSec=10
+User=pi
+ExecStart=/home/pi/wallpaper.sh $(cat /home/pi/wallpaper-option.txt)
+StandardOutput=inherit
+StandardError=journal
+
+[Install]
+WantedBy=multi-user.target
+EOF
+
+sudo systemctl daemon-reload
+sudo systemctl enable wallpaper
